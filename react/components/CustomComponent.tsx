736eed26821838cdf87c15ac4fa3c98b7b817fdf
mauroxcf <maurox03@gmail.com>
2023-04-20 16:14:19 -0500

    feature: adding geolocalization component

--- react/components/CustomComponent.tsx
+++ react/components/CustomComponent.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+/* import React from 'react' */
+import React, { useEffect, useState } from 'react'
+import { useGeolocated } from 'react-geolocated'
 
 const CustomComponent = () => {
+  const { isGeolocationAvailable, isGeolocationEnabled, coords } = useGeolocated({
+    positionOptions: {
+        enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 5000,
+})
+  const [locationData, setLocationData] = useState({city: "", countryName: ""})
+
+  useEffect(() => {
+    if (coords) {
+      fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${coords.latitude}&longitude=${coords.longitude}&localityLanguage=en`)
+        .then(response => response.json())
+        .then(data => setLocationData(data))
+    }
+  }, [coords])
+
   return (
-    <div>CustomComponent</div>
+    <div>
+      {isGeolocationAvailable
+        ? `Geolocation is available`
+        : `Geolocation is not available`}
+      <br />
+      {isGeolocationEnabled
+        ? `Geolocation is enabled`
+        : `Geolocation is not enabled`}
+      <br />
+      {coords
+        ? `Latitude: ${coords.latitude}, Longitude: ${coords.longitude}`
+        : `Waiting for location...`}
+      <br />
+      {locationData
+        ? `City: ${locationData?.city}, Country: ${locationData?.countryName}`
+        : null}
+    </div>
   )
 }
 
